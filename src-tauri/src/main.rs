7bcf9aaa382fc0a7cc69b6a616dc88509b90de42
Adam <awhiles2011@gmail.com>
2024-02-01 15:25:19 -0600

    add localhost plugin to work with azure app in prod

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{utils::config::AppUrl, window::WindowBuilder, WindowUrl};
 
 fn main() {
+    let port = portpicker::pick_unused_port().expect("failed to find unused port");
+
+    let mut context = tauri::generate_context!();
+    let url = format!("http://localhost:{}", port).parse().unwrap();
+    let window_url = WindowUrl::External(url);
+    // rewrite the config so the IPC is enabled on this URL
+    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .plugin(tauri_plugin_store::Builder::default().build())
-        .run(tauri::generate_context!())
+        .plugin(tauri_plugin_localhost::Builder::new(port).build())
+        .setup(move |app| {
+            // use "localhost" as the window label
+            WindowBuilder::new(
+                app,
+                "main".to_string(),
+                if cfg!(dev) {
+                    Default::default()
+                } else {
+                    window_url
+                },
+            )
+            .fullscreen(false)
+            .resizable(true)
+            .title("ADAM-Tool-V2")
+            .inner_size(1145.0, 750.0)
+            .min_inner_size(800.0, 750.0)
+            .build()?;
+            Ok(())
+        })
+        .run(context)
         .expect("error while running tauri application");
 }

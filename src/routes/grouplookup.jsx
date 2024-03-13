f0509d8024dab52b1d32ad0c405381d52eeafa9f
Adam <awhiles2011@gmail.com>
2024-03-13 10:58:56 -0500

    Feat - Add error handling for grouplookup API call and error modal logic

--- src/routes/grouplookup.jsx
+++ src/routes/grouplookup.jsx
@@ -8,17 +8,31 @@ import { unstable_useViewTransitionState } from "react-router-dom";
 export default function GroupLookup() {
   const [gGroupName, sGroupName] = useState();
   const [getGroupInfo, setGroupInfo] = useState();
-  const [showModal, setShowModal] = useState(false); // New state variable for controlling the modal visibility
+  const [showModal, setShowModal] = useState(false);
+  const [getError, setError] = useState();
 
   const lookupGroup = async () => {
-    if (!gGroupName) { // Check if the input field is empty
-      setShowModal(true); // Show the modal
-      return; // Return to prevent the rest of the code from executing
+    // Trigger error modal if no group name is entered
+    if (!gGroupName) {
+      setError('Please enter a group name to lookup');
+      setShowModal(true); 
+      return; 
     }
-    const client = Providers.globalProvider.graph.client;
-    const response = await client.api(`/groups?$filter=(displayName eq '${gGroupName}')&$select=id,displayName,groupTypes,membershipRule,members`).get();
-    console.log(response);
-    setGroupInfo(response);
+    // Call Graph API to get group information
+    try {
+      const client = Providers.globalProvider.graph.client;
+      const response = await client.api(`/groups?$filter=(displayName eq '${gGroupName}')&$select=id,displayName,groupTypes,membershipRule,members`).get();
+      // Check that a group was found
+      if (!response.value || response.value.length === 0) {
+        throw new Error('No group found with that name. Please try again.');
+      } else {
+        // Set group state variable with API response
+        setGroupInfo(response);
+      }      
+    } catch (error) {
+      setError(error.message);
+      setShowModal(true); 
+    }
   };
 
   return (
@@ -31,7 +45,7 @@ export default function GroupLookup() {
       {getGroupInfo ? (
         <GroupInfo groupInfo={getGroupInfo} />
       ) : null}
-      {showModal ? <Modal onClose={() => setShowModal(false)}>Empty Group</Modal> : null}
+      {showModal ? <Modal onClose={() => setShowModal(false)}>{getError}</Modal> : null}
     </div>
   );
 }
